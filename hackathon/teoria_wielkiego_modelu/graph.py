1795d7e4923f50c372c592ecf1d4ac3c15c28cef
Kyrylo <kiri.goroshenko@gmail.com>
2025-04-14 17:10:01 +0200

    graph 2

--- hackathon/teoria_wielkiego_modelu/graph.py
+++ hackathon/teoria_wielkiego_modelu/graph.py
@@ -32,7 +32,6 @@ def improve_hypothesis(
         logger.info("Hypothesis rejected after {} iterations", state["iteration"])
         return "hypothesis_refiner"
 
-
 def create_hypgen_graph() -> CompiledGraph:
     graph = StateGraph(HypgenState)
 
@@ -80,12 +79,46 @@ def create_hypgen_graph() -> CompiledGraph:
     graph.add_edge("critique_analyst", "review_agent")
     graph.add_edge("devil_advocate", "review_agent")
 
-    # graph.add_edge("critique_analyst", END)
-    graph.add_conditional_edges(
-        "critique_analyst",
-        improve_hypothesis,
+    graph.add_edge("review_agent", "summary_agent")
+
+    return graph.compile()
+
+def refine_graph() -> CompiledGraph:
+    graph = StateGraph(HypgenState)
+
+    graph.add_node(
+        "hypothesis_refiner", create_hypothesis_refiner_agent("small")["agent"]
     )
-    graph.add_edge("summary_agent", END)
+    graph.add_node("literature_agent", create_literature_agent("small")["agent"])
+    graph.add_node("inspiration_agent", create_inspiration_agent("small")["agent"])
+    graph.add_node("nai_agent", create_nai_agent("small")["agent"])
+    graph.add_node("exp_planer", create_experiment_planner_agent("small")["agent"])
+    graph.add_node("exp_reviewer", create_experiment_reviewer_agent("small")["agent"])
+    graph.add_node("critique_analyst", create_critique_agent("small")["agent"])
+    graph.add_node("devil_advocate", create_devil_advocate_agent("small")["agent"])
+    graph.add_node("summary_agent", create_summary_agent("small")["agent"])
+
+    # Add edges
+    graph.add_edge(START, "hypothesis_refiner")
+
+    graph.add_edge("hypothesis_refiner", "literature_agent")
+    # # Fork
+    graph.add_edge("literature_agent", "nai_agent")
+    graph.add_edge("literature_agent", "exp_planer")
+    graph.add_edge("exp_planer", "exp_agent")
+    # # Join
+    graph.add_edge("nai_agent", "critique_analyst")
+    graph.add_edge("nai_agent", "devil_advocate")
+
+    graph.add_edge("exp_planer", "critique_analyst")
+    graph.add_edge("exp_planer", "devil_advocate")
+
+    graph.add_edge("exp_reviewer", "critique_analyst")
+    graph.add_edge("exp_reviewer", "devil_advocate")
+
+    graph.add_edge("critique_analyst", "review_agent")
+    graph.add_edge("devil_advocate", "review_agent")
+    graph.add_edge("review_agent", "summary_agent")
 
     return graph.compile()
 
